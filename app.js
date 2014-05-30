8a7aff987e02b2242f812e0f2f795ce5812b4a61
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2014-05-30 22:48:46 +0800

    Added New Relic Browser Logging

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('newrelic');
-require('prerender-node');
+rupp = {};
+rupp.newrelic = require('newrelic');
 // Start sails and pass it command line arguments
 require('sails').lift(require('optimist').argv);
